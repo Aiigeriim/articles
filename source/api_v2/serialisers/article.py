4ad54240ec6642998775fd0e4f1037234d28a15b
Aiigeriim <sardarbekova1005@gmail.com>
2025-08-25 19:11:39 +0300

    возможно получение статей и создание статей через постман,

---
+++ source/api_v2/serialisers/article.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+
+from webapp.models.article import statuses
+from webapp.models import Tag, Article
+
+
+class ArticleSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(required=True, max_length=50)
+    content = serializers.CharField(required=True)
+    author = serializers.PrimaryKeyRelatedField(read_only=True)
+    status = serializers.ChoiceField(choices=statuses, default=statuses[0][0])
+    tags = serializers.PrimaryKeyRelatedField(many=True, queryset=Tag.objects.all(), required=False)
+    created_at = serializers.DateTimeField(read_only=True)
+    updated_at = serializers.DateTimeField(read_only=True)
+
+    def create(self, validated_data):
+        tags = validated_data.pop('tags')
+        article = Article.objects.create(**validated_data)
+        article.tags.set(tags)
+        return article
+
+
+
--- source/api_v2/views.py
+++ source/api_v2/views.py
@@ -1,7 +1,13 @@
-from django.http import HttpResponse, HttpResponseNotAllowed
-from django.shortcuts import render
+import json
+from json import JSONDecodeError
 
+from django.contrib.auth import get_user_model
+from django.http import HttpResponse, HttpResponseNotAllowed, JsonResponse, HttpResponseBadRequest
+from django.views import View
 from django.views.decorators.csrf import ensure_csrf_cookie
+from webapp.models.article import Article
+
+from api_v2.serialisers import ArticleSerializer
 
 
 @ensure_csrf_cookie
@@ -9,3 +15,24 @@ def get_token_view(request, *args, **kwargs):
     if request.method == 'GET':
         return HttpResponse()
     return HttpResponseNotAllowed('Only GET request are allowed')
+
+
+class ArticleView(View):
+    def get(self, request, *args, **kwargs):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    def post(self, request, *args, **kwargs):
+        body = json.loads(request.body)
+        serializer = ArticleSerializer(data=body)
+        if serializer.is_valid():
+            # user = request.user
+            user = get_user_model().objects.last()
+            article = serializer.save(author=user)
+            return JsonResponse({'id': article.id}, status=201)
+        else:
+            return JsonResponse({'error': serializer.errors}, status=400)
+
+
+
